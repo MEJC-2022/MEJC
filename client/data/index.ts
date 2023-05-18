70d6aa8614331bcc9266d394a2f2749dbadfab2e
Emil Helgesson <helgesson.emil.94@gmail.com>
2023-05-18 17:09:20 +0200

    change back to title

--- client/data/index.ts
+++ client/data/index.ts
@@ -1,7 +1,7 @@
 export interface Product {
   id: string;
   image: string;
-  name: string;
+  title: string;
   description: string;
   summary: string[];
   price: number;
@@ -17,7 +17,7 @@ export const products: Product[] = [
     id: '1',
     image:
       'https://cdn.pixabay.com/photo/2016/11/19/15/32/laptop-1839876_960_720.jpg',
-    name: 'UltraBook X9',
+    title: 'UltraBook X9',
     description:
       'The UltraBook X9 is the ultimate productivity machine for professionals on-the-go. With its lightweight design and powerful hardware, you can breeze through any task without breaking a sweat. The X9 also features a stunning 15-inch display with 4K resolution, perfect for streaming your favorite shows or editing photos.',
     summary: ['Lightweight design', 'Powerful hardware', '15-inch 4K display'],
@@ -28,7 +28,7 @@ export const products: Product[] = [
     id: '2',
     image:
       'https://images.unsplash.com/photo-1629131726692-1accd0c53ce0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
-    name: 'PowerBook 13',
+    title: 'PowerBook 13',
     description:
       'The PowerBook 13 is the perfect laptop for students and creative professionals. With its compact size and powerful performance, you can take your work wherever you go. The 13-inch Retina display delivers crisp and clear visuals, while the powerful processor ensures speedy performance.',
     summary: [
@@ -43,7 +43,7 @@ export const products: Product[] = [
     id: '3',
     image:
       'https://images.pexels.com/photos/7974/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1600',
-    name: 'GamingBook Pro',
+    title: 'GamingBook Pro',
     description:
       'The GamingBook Pro is the ultimate gaming machine for hardcore gamers. With its powerful graphics card and lightning-fast processor, you can play the latest games at the highest settings. The 17-inch Full HD display delivers immersive visuals, while the backlit keyboard ensures you can keep gaming even in low light.',
     summary: [
@@ -58,7 +58,7 @@ export const products: Product[] = [
     id: '4',
     image:
       'https://images.pexels.com/photos/129208/pexels-photo-129208.jpeg?auto=compress&cs=tinysrgb&w=1600',
-    name: 'BookAir S',
+    title: 'BookAir S',
     description:
       'The BookAir S is the perfect laptop for those who value style and portability. With its slim and lightweight design, you can take it with you wherever you go. The 14-inch Full HD display delivers stunning visuals, while the long-lasting battery ensures you can work or play all day.',
     summary: [
@@ -73,7 +73,7 @@ export const products: Product[] = [
     id: '5',
     image:
       'https://images.pexels.com/photos/669228/pexels-photo-669228.jpeg?auto=compress&cs=tinysrgb&w=1600',
-    name: 'WorkBook Plus',
+    title: 'WorkBook Plus',
     description:
       'The WorkBook Plus is the ultimate workstation for professionals who need to run demanding applications. With its powerful hardware and spacious 17-inch display, you can work on multiple projects at once without any lag. The workstation also features a backlit keyboard for typing in low light conditions.',
     summary: [
@@ -88,7 +88,7 @@ export const products: Product[] = [
     id: '6',
     image:
       'https://images.unsplash.com/flagged/1/apple-gear-looking-pretty.jpg?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
-    name: 'TravelBook Mini',
+    title: 'TravelBook Mini',
     description:
       'The TravelBook Mini is the perfect laptop for those who are always on-the-go. With its compact size and long-lasting battery, you can take it with you wherever you go. The 11-inch display delivers crisp visuals, while the lightweight design ensures you can carry it with ease.',
     summary: ['Long-lasting battery', '11-inch display', 'Lightweight design'],
