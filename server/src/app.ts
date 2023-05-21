aeef458efa51e7e2ab2871a91553cda170731c15
Emil Helgesson <helgesson.emil.94@gmail.com>
2023-05-21 19:36:36 +0200

    add global middware, error-handler and routers

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,16 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import productRouter from './routers/product-router';
 
 export const app = express();
 
-app.listen();
+// Global middlewares
+app.use(express.json());
+
+// Routers:
+app.use(productRouter);
+
+// Global error-handling:
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.error(err);
+  res.status(500).json(err.message);
+});
