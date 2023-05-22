0347535e19abbc0158a4786be234ff1b7b77f798
lysmac <calle@lysmask.se>
2023-05-22 14:30:23 +0200

    can now create an order from the site into the DB

--- server/src/controllers/order-controller.ts
+++ server/src/controllers/order-controller.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from 'express';
+import { OrderModel } from '../models/order-model';
 
 export async function createOrder(req: Request, res: Response) {
   console.log('reached createOrder');
   const address = req.body.address;
   const products = req.body.orderItems;
   let haveArchivedProduct = false;
+  const userId = '5f9d3b3b9d3b3b9d3b9d3b9d';
 
   // Check if product is archived not depending on req
   // but in the database instead?
@@ -13,9 +15,6 @@ export async function createOrder(req: Request, res: Response) {
   //     haveArchivedProduct = true;
   //   }
   // });
-  console.log(req.body);
-  // console.log(address);
-  // console.log(products);
 
   if (haveArchivedProduct) {
     res.status(400).send({
@@ -23,6 +22,18 @@ export async function createOrder(req: Request, res: Response) {
     });
     return;
   }
+  const completOrder = {
+    orderId: '123884954589',
+    userId: userId,
+    deliveryAddress: address,
+    orderItems: products,
+    createdAt: new Date(),
+    isShipped: false,
+    totalPrice: 0,
+  };
+
+  const result = await OrderModel.create(completOrder);
+
   res.status(200).send({
     message: 'Order created successfully.',
   });
