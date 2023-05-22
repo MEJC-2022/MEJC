61e8b576f3767aa17f25f621342a3ffb907135ff
Emil Helgesson <helgesson.emil.94@gmail.com>
2023-05-22 12:39:40 +0200

    Add initial setup to upload files (WIP)

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,5 @@
 import express, { NextFunction, Request, Response } from 'express';
+import fileRouter from './routers/file-router';
 import productRouter from './routers/product-router';
 
 export const app = express();
@@ -8,6 +9,7 @@ app.use(express.json());
 
 // Routers:
 app.use(productRouter);
+app.use(fileRouter);
 
 // Global error-handling:
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
