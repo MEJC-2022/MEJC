b5a95fc64865bae2f486eb56fc52a4a0653bbb91
Emil Helgesson <helgesson.emil.94@gmail.com>
2023-05-22 20:59:26 +0200

    add rest files &  remaining product endpoints

--- server/src/controllers/product-controller.ts
+++ server/src/controllers/product-controller.ts
@@ -12,4 +12,45 @@ const getAllProducts = async (req: Request, res: Response) => {
   res.json(products);
 };
 
-export { createProduct, getAllProducts };
+const getProductById = async (req: Request, res: Response) => {
+  const product = await ProductModel.findById(req.params.id);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).send({ message: 'Product not found' });
+  }
+};
+
+const updateProduct = async (req: Request, res: Response) => {
+  const product = await ProductModel.findById(req.params.id);
+  if (product) {
+    product.image = req.body.image;
+    product.title = req.body.title;
+    product.description = req.body.description;
+    product.price = req.body.price;
+    product.stock = req.body.stock;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } else {
+    res.status(404).send({ message: 'Product not found' });
+  }
+};
+
+const deleteProduct = async (req: Request, res: Response) => {
+  const product = await ProductModel.findById(req.params.id);
+  if (product) {
+    await product.deleteOne();
+    res.send({ message: 'Product deleted' });
+  } else {
+    res.status(404).send({ message: 'Product not found' });
+  }
+};
+
+export {
+  createProduct,
+  getAllProducts,
+  getProductById,
+  updateProduct,
+  deleteProduct,
+};
--- server/src/routers/product-router.ts
+++ server/src/routers/product-router.ts
@@ -1,12 +1,18 @@
 import express from 'express';
 import {
   createProduct,
+  deleteProduct,
   getAllProducts,
+  getProductById,
+  updateProduct,
 } from '../controllers/product-controller';
 
 const productRouter = express.Router();
 
 productRouter.post('/api/products', createProduct);
 productRouter.get('/api/products', getAllProducts);
+productRouter.get('/api/products/:id', getProductById);
+productRouter.put('/api/products/:id', updateProduct);
+productRouter.delete('/api/products/:id', deleteProduct);
 
 export default productRouter;
