9aaa3b4e51116365f8ef53cf54a18247cd15b6b7
stenbumling <marcusrosin@outlook.com>
2023-05-24 08:43:55 +0200

    refactor: improved error handling on admin handler

--- server/src/handlers/admin-handler.ts
+++ server/src/handlers/admin-handler.ts
@@ -1,13 +1,14 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from 'express';
 
 export function isAdmin(req: Request, res: Response, next: NextFunction) {
   try {
     if (req.session && req.session.user && req.session.user.isAdmin) {
       next();
     } else {
-      res.status(403).json('You are not authorized.');
+      res.status(403).json('You do not have access to this resource');
     }
   } catch (err) {
-    console.error(err);
+    console.error('An error has occurred during admin authorization:\n', err);
+    res.status(500).json({ error: 'Admin status could not be verified' });
   }
 }
