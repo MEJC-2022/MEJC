8b2eb80dbcbd89433a74d9566b3537f581d73215
stenbumling <marcusrosin@outlook.com>
2023-05-25 10:52:56 +0200

    feat: redirects logged in users to previous route if trying to access /signin

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -1,12 +1,23 @@
 import { Center, Title, useMantineTheme } from '@mantine/core';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import SignInForm from '../components/SignInForm';
+import { useAuth } from '../contexts/AuthContext';
 import { formStyle } from '../css/formStyle';
 
 export function SignIn() {
   const theme = useMantineTheme();
   const { classes } = formStyle();
+  const { isSignedIn } = useAuth();
+  const navigate = useNavigate();
 
-  return (
+  useEffect(() => {
+    if (isSignedIn) {
+      navigate(-1);
+    }
+  }, [isSignedIn, navigate]);
+
+  return !isSignedIn ? (
     <Center className={classes.wrapper}>
       <Title
         className={`${classes.title} ${
@@ -17,5 +28,7 @@ export function SignIn() {
       </Title>
       <SignInForm />
     </Center>
+  ) : (
+    <div>It looks like you are already logged in. Redirecting...</div>
   );
 }
