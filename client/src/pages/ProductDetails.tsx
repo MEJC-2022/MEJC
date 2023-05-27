3dbf78b8bb7dfe4bfd5db2e3b280a26e4281dd61
Emil Helgesson <helgesson.emil.94@gmail.com>
2023-05-27 19:44:06 +0200

    display stock on product detail page

--- client/src/pages/ProductDetails.tsx
+++ client/src/pages/ProductDetails.tsx
@@ -1,4 +1,5 @@
 import {
+  Badge,
   Box,
   Button,
   Card,
@@ -77,6 +78,9 @@ function ProductDetails() {
             alt={product.title}
             fit="contain"
           />
+          <Badge variant="gradient" pos="absolute" top="30%" right="5%">
+            {product.stock} in stock
+          </Badge>
         </Card>
         <Card sx={{ flex: 1 }}>
           <Box
