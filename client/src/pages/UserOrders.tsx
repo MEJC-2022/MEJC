3590d77d35a755e36091ce54e9b812beb2b9ab7d
stenbumling <marcusrosin@outlook.com>
2023-05-28 17:14:41 +0200

    style: added backgrounds to user and admin orders

--- client/src/pages/UserOrders.tsx
+++ client/src/pages/UserOrders.tsx
@@ -17,7 +17,7 @@ const useStyles = createStyles((theme) => ({
     backgroundImage:
       theme.colorScheme === 'dark'
         ? `linear-gradient(-60deg, ${theme.colors.gray[8]} 0%, ${theme.colors.gray[9]} 100%)`
-        : `linear-gradient(-60deg, ${theme.colors.gray[3]} 0%, ${theme.colors.gray[1]} 100%)`,
+        : `linear-gradient(-60deg, ${theme.colors.blue[3]} 0%, ${theme.colors.blue[7]} 100%)`,
     padding: `calc(${theme.spacing.xl} * 5)`,
     minHeight: 'calc(100vh - 4.375rem - 10rem)',
     [theme.fn.smallerThan('sm')]: {
@@ -34,7 +34,7 @@ const useStyles = createStyles((theme) => ({
     color:
       theme.colorScheme === 'dark'
         ? theme.colors.blue[5]
-        : theme.colors.gray[7],
+        : theme.colors.gray[1],
     lineHeight: 1,
     marginBottom: `calc(${theme.spacing.xl} * 1.5)`,
   },
