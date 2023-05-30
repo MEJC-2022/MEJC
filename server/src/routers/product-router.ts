862e4eafdb456ca6b5e1568855bb5d8a9e82b008
stenbumling <marcusrosin@outlook.com>
2023-05-30 08:52:47 +0200

    feat: add auth checks for product endpoints

--- server/src/routers/product-router.ts
+++ server/src/routers/product-router.ts
@@ -7,15 +7,16 @@ import {
   getProductById,
   updateProduct,
 } from '../controllers/product-controller';
+import { isAdmin } from '../middlewares/auth-checks';
 
 const productRouter = express.Router();
 
 // TODO: Add AUTH to this route
-productRouter.post('/api/products', createProduct);
+productRouter.post('/api/products', isAdmin, createProduct);
 productRouter.get('/api/products', getAllProducts);
 productRouter.get('/api/products/created', getAllCreatedProducts);
 productRouter.get('/api/products/:id', getProductById);
-productRouter.put('/api/products/:id', updateProduct);
-productRouter.delete('/api/products/:id', deleteProduct);
+productRouter.put('/api/products/:id', isAdmin, updateProduct);
+productRouter.delete('/api/products/:id', isAdmin, deleteProduct);
 
 export default productRouter;
