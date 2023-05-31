6d8a440003fa5c2e3338b760fec01f98cd68d17e
Jenny Pettersson <j.m.pettersson@hotmail.se>
2023-05-31 16:30:19 +0200

    applied marcus styling to banner

--- client/src/components/SVGBanner.tsx
+++ client/src/components/SVGBanner.tsx
@@ -1,44 +1,120 @@
-import { Box, MediaQuery, useMantineTheme } from "@mantine/core";
+import {
+  Box,
+  Image,
+  Text,
+  createStyles,
+  useMantineTheme
+} from '@mantine/core';
+import { useEffect, useState } from 'react';
+
+const images = [
+  {
+    fileName: 'recycling',
+    textAbove: 'Recyclable',
+    textBelow: 'Parts',
+  },
+  {
+    fileName: 'sustainability',
+    textAbove: 'Sustainable',
+    textBelow: 'Transport',
+  },
+  {
+    fileName: 'eustars',
+    textAbove: 'Free Shipping',
+    textBelow: 'Within EU',
+  },
+  {
+    fileName: 'wallet',
+    textAbove: 'Price',
+    textBelow: 'Guarantee',
+  },
+  {
+    fileName: 'return',
+    textAbove: 'Free Returns',
+    textBelow: 'Within 30 Days',
+  },
+];
+
+const useStyles = createStyles((theme) => ({
+  responsiveImg: {
+    filter:
+      'brightness(0) saturate(100%) invert(52%) sepia(96%) saturate(5788%) hue-rotate(206deg) brightness(88%) contrast(98%)',
+    [theme.fn.smallerThan('sm')]: {
+    },
+  },
+  textSize: {
+    fontSize: 14,
+    [theme.fn.smallerThan('sm')]: {
+      fontSize: 10,
+    },
+  }
+}));
+
 
 export function SVGBanner() {
-    const theme = useMantineTheme();
-    
-    return (
-        <MediaQuery
-        query="(max-width: 650px)"
-        styles={{
-          img: {
-            width: '3.6rem',
-            height: '3.6rem',
-          },
+  const theme = useMantineTheme();
+  const { classes } = useStyles();
+  const [imageSize, setImageSize] = useState('40px');
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        setImageSize('20px'); 
+      } else {
+        setImageSize('40px'); 
+      }
+    };
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return (
+      <Box
+        sx={{
+          width: '100%',
+          background:
+            theme.colorScheme === 'dark'
+              ? theme.colors.dark[6]
+              : theme.colors.blue[0],
+          display: 'flex',
+          justifyContent: 'space-around',
+          marginTop: '0rem',
+          padding: '.3rem',
+          boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+          border: theme.colorScheme === 'light' ? '1px #EEEEEE solid' : 'none',
         }}
       >
-        <Box
-          sx={{
-            width: '100%',
-            background:
-              theme.colorScheme === 'dark'
-                ? theme.colors.dark[0]
-                : theme.colors.blue[0],
-            display: 'flex',
-            justifyContent: 'space-around',
-            marginBottom: '1.5rem',
-            marginTop: '0rem',
-            padding: '.3rem',
-            boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-            border:
-              theme.colorScheme === 'light' ? '1px #EEEEEE solid' : 'none',
-          }}
-        >
-          <img src="/assets/recycable-parts.svg" alt="recycable parts icon" />
-          <img
-            src="./assets/sustainable-transports.svg"
-            alt="sustainable transports icon"
-          />
-          <img src="/assets/free-deliveries.svg" alt="free deliveries icon" />
-          <img src="/assets/price-guarantee.svg" alt="price guarantee icon" />
-          <img src="/assets/free-returns.svg" alt="free returns icon" />
-        </Box>
-      </MediaQuery>
-    )
+        {images.map(({ fileName, textAbove, textBelow }) => (
+          <div
+            key={fileName}
+            style={{
+              marginBottom: '0px',
+              textAlign: 'center',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <Text align="center" fw={400} className={classes.textSize} mb={3}>
+              {textAbove}
+            </Text>
+            <Image
+              src={`assets/${fileName}.svg`}
+              alt={`Image ${fileName}`}
+              width= {imageSize}
+              height= {imageSize}
+              className={classes.responsiveImg}
+              mb={3}
+            />
+            <Text align="center" fw={400} className={classes.textSize}>
+              {textBelow}
+            </Text>
+          </div>
+        ))}
+      </Box>
+  );
 }
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -7,7 +7,7 @@ import {
   SimpleGrid,
   Text,
   Title,
-  useMantineTheme
+  useMantineTheme,
 } from '@mantine/core';
 import { useContext, useEffect, useState } from 'react';
 import HeroSlide from '../components/HeroSlide';
@@ -46,100 +46,94 @@ function Home() {
 
   return (
     <>
-    <HeroSlide />
-    <SVGBanner />
-    <Container size="xl" py={'3rem'}>
-      <Box
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          flexDirection: 'column',
-        }}
-      >
+      <HeroSlide />
+      <SVGBanner />
+      <Container size="xl" py={'3rem'}>
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            flexDirection: 'column',
+          }}
+        >
+          <Title>Tech101</Title>
+          <Text fz="xl" fw={500}>
+            Providing up-to-date <br /> products and services
+          </Text>
+          <Text fz="lg" fs="italic">
+            We. Tech. You
+          </Text>
+        </Box>
         <Title
+          sx={{ marginBottom: '1rem' }}
+          ta="center"
           className={theme.colorScheme === 'dark' ? 'neonText' : ''}
-          mt={40}
-          mb={14}
-          size={70}
         >
-          Tech101
+          Browse our collection
         </Title>
-        <Text fz="xl" fw={500}>
-          Providing up-to-date <br /> products and services
-        </Text>
-        <Text fz="lg" fs="italic">
-          We. Tech. You.
-        </Text>
-      </Box>
-      <Title
-        sx={{ marginTop: '4rem', marginBottom: '3rem' }}
-        ta="center"
-        className={theme.colorScheme === 'dark' ? 'neonText' : ''}
-      >
-        Browse our collection
-      </Title>
-      <Text
-        sx={{ marginBottom: '1rem', marginLeft: '0.6rem', fontSize: '0.8rem' }}
-        fw={700}
-        color="none"
-        size="lg"
-      >
-        SORT BY CATEGORY:
-      </Text>
-      <Group spacing={5} mb="md">
-        <Button
-          sx={{
-            border:
-              activeButton === 'Laptops' ? '2px solid lightblue ' : 'none',
-          }}
-          size="xs"
-          variant="light"
-          radius="sm"
-          onClick={() => filterByCategory('Laptops')}
-        >
-          Laptops
-        </Button>
-        <Button
-          sx={{
-            border: activeButton === 'Apple' ? '2px solid lightblue ' : 'none',
-          }}
-          size="xs"
-          variant="light"
-          radius="sm"
-          onClick={() => filterByCategory('Apple')}
-        >
-          Apple Products
-        </Button>
-        <Button
-          sx={{
-            border: activeButton === 'Asus' ? '2px solid lightblue ' : 'none',
-          }}
-          size="xs"
-          variant="light"
-          radius="sm"
-          onClick={() => filterByCategory('Asus')}
-        >
-          Asus Products
-        </Button>
-        <Button size="xs" variant="outlined" radius="sm" onClick={resetFilter}>
-          Reset filter
-        </Button>
-      </Group>
+        <Badge sx={{ marginBottom: '1rem' }} size="lg" radius="sm">
+          Sort By Category:
+        </Badge>
+        <Group spacing={5} mb="md">
+          <Button
+            sx={{
+              border:
+                activeButton === 'Laptops' ? '2px solid lightblue ' : 'none',
+            }}
+            size="xs"
+            variant="light"
+            radius="sm"
+            onClick={() => filterByCategory('Laptops')}
+          >
+            Laptops
+          </Button>
+          <Button
+            sx={{
+              border:
+                activeButton === 'Apple' ? '2px solid lightblue ' : 'none',
+            }}
+            size="xs"
+            variant="light"
+            radius="sm"
+            onClick={() => filterByCategory('Apple')}
+          >
+            Apple Products
+          </Button>
+          <Button
+            sx={{
+              border: activeButton === 'Asus' ? '2px solid lightblue ' : 'none',
+            }}
+            size="xs"
+            variant="light"
+            radius="sm"
+            onClick={() => filterByCategory('Asus')}
+          >
+            Asus Products
+          </Button>
+          <Button
+            size="xs"
+            variant="outlined"
+            radius="sm"
+            onClick={resetFilter}
+          >
+            Reset filter
+          </Button>
+        </Group>
 
-      <SimpleGrid
-        cols={3}
-        spacing="xl"
-        verticalSpacing="xl"
-        breakpoints={[
-          { maxWidth: '85rem', cols: 2, spacing: 'md' },
-          { maxWidth: '54rem', cols: 1, spacing: 'sm' },
-        ]}
-      >
-        {filteredProducts.map((product) => (
-          <ProductCard key={product._id} product={product} />
-        ))}
-      </SimpleGrid>
-    </Container>
+        <SimpleGrid
+          cols={3}
+          spacing="xl"
+          verticalSpacing="xl"
+          breakpoints={[
+            { maxWidth: '85rem', cols: 2, spacing: 'md' },
+            { maxWidth: '36rem', cols: 1, spacing: 'sm' },
+          ]}
+        >
+          {filteredProducts.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+        </SimpleGrid>
+      </Container>
     </>
   );
 }
