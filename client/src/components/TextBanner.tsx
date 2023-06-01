13ff3bab9a25485471d1b68c01cf26fcee275440
Jenny Pettersson <j.m.pettersson@hotmail.se>
2023-06-01 09:13:54 +0200

    textbanner

--- client/src/components/TextBanner.tsx
+++ client/src/components/TextBanner.tsx
@@ -1,38 +1,104 @@
 import { Container, Text, createStyles, useMantineTheme } from '@mantine/core';
+import { IconHome, IconTruck } from '@tabler/icons-react';
 
 const useStyles = createStyles((theme) => ({
   container: {
     width: '100%',
     paddingBottom: '0.8rem',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
   },
   middleTextStyling: {
-    textTransform: 'capitalize',
     fontSize: 40,
-    wordSpacing: '2px',
     [theme.fn.smallerThan('sm')]: {
       fontSize: 20,
     },
   },
   textSpacing: {
     margin: '0 10px',
+    [theme.fn.smallerThan('sm')]: {
+      margin: '0 3px',
+    },
+  },
+  iconStyling: {
+    [theme.fn.smallerThan('sm')]: {
+      display: 'none',
+    },
   },
 }));
 
 export function TextBanner() {
   const { classes } = useStyles();
   const theme = useMantineTheme();
-  
+
   return (
     <Container className={classes.container}>
-      <Text align="center" color={theme.colorScheme === 'dark' ? theme.colors.gray[7] : theme.colors.dark[8]} fw={600} className={classes.middleTextStyling}>
+      <Text
+        color={
+          theme.colorScheme === 'dark'
+            ? theme.colors.dark[3]
+            : theme.colors.dark[8]
+        }
+        fw={600}
+        className={classes.middleTextStyling}
+      >
         <span className={classes.textSpacing}>
-          <span className={theme.colorScheme === 'dark' ? 'blueNeonText' : ''}>We</span> bring
-        </span>{' '}
+          <span className={theme.colorScheme === 'dark' ? 'blueNeonText' : ''}>
+            We
+          </span>{' '}
+          Bring
+        </span>
+      </Text>
+      <IconTruck
+        size={30}
+        className={classes.iconStyling}
+        color={
+          theme.colorScheme === 'dark'
+            ? theme.colors.dark[3]
+            : theme.colors.dark[8]
+        }
+      />
+      <Text
+        color={
+          theme.colorScheme === 'dark'
+            ? theme.colors.dark[3]
+            : theme.colors.dark[8]
+        }
+        fw={600}
+        className={classes.middleTextStyling}
+      >
         <span className={classes.textSpacing}>
-          the <span className={theme.colorScheme === 'dark' ? 'pinkNeonText' : ''}>Tech</span>
-        </span>{' '}
+          The{' '}
+          <span className={theme.colorScheme === 'dark' ? 'pinkNeonText' : ''}>
+            Tech
+          </span>
+        </span>
+      </Text>
+      <IconHome
+        size={30}
+        className={classes.iconStyling}
+        color={
+          theme.colorScheme === 'dark'
+            ? theme.colors.dark[3]
+            : theme.colors.dark[8]
+        }
+      />
+      <Text
+        color={
+          theme.colorScheme === 'dark'
+            ? theme.colors.dark[3]
+            : theme.colors.dark[8]
+        }
+        fw={600}
+        className={classes.middleTextStyling}
+      >
         <span className={classes.textSpacing}>
-          home to <span className={theme.colorScheme === 'dark' ? 'blueNeonText' : ''}>You</span>
+          Home To{' '}
+          <span className={theme.colorScheme === 'dark' ? 'blueNeonText' : ''}>
+            You
+          </span>
         </span>
       </Text>
     </Container>
